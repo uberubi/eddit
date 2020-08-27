646dbf566ac6b682dbb4ce576f5ee507add9b11d
Andrew A. Stenipanin <and5251@yandex.ru>
2020-08-28 01:11:02 +0300

    MirkoORM setup

---
+++ dist/psql_config.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.dbPassword = exports.dbName = void 0;
+exports.dbName = "eddit";
+exports.dbPassword = "123456";
+//# sourceMappingURL=psql_config.js.map
