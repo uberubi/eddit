42b812dbbb75d9edb53f5159b2e5b2a41d50a645
Andrew A. Stenipanin <and5251@yandex.ru>
2020-08-31 00:25:51 +0300

    register error handling

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -4,18 +4,25 @@ import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box, Button } from "@chakra-ui/core";
 import { useRegisterMutation } from "../generated/graphql";
-
+import { toErrorMap } from "../utils/toErrorMap";
+import {useRouter} from 'next/router'
 interface registerProps {}
 
 export const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useRegisterMutation()
+  const router= useRouter()
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
-          const response = await register(values)
-          if (response.data.register.errorserror)
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            //worked
+            router.push('/')
+          }
         }}
       >
         {({ isSubmitting }) => (
@@ -47,6 +54,5 @@ export const Register: React.FC<registerProps> = ({}) => {
     </Wrapper>
   );
 };
-
 
 export default Register;
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Query,
 } from "type-graphql";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class UsernamePasswordInput {
@@ -42,7 +43,7 @@ export class UserResolver {
   @Query(() => User, { nullable: true })
   async me(@Ctx() { req, em }: MyContext) {
     // you are not logged in
-    console.log(req.session)
+    console.log(req.session);
     if (!req.session.userId) {
       return null;
     }
@@ -79,11 +80,19 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
     try {
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+        user = result[0]
       await em.persistAndFlush(user);
     } catch (err) {
       //|| err.detail.includes("already exists")) {
